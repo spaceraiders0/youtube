7cb8de717d112a71e2c4ad89136498e912e3f899
spaceraiders0 <spaceraiders@tutanota.com>
2021-03-07 13:56:41 +0000

    Cleaned up argument definitions.

--- src/youtube.py
+++ src/youtube.py
@@ -4,6 +4,7 @@
 """
 
 import sys
+import clipboard
 import constants
 import definitions
 from argparse import ArgumentParser
@@ -18,25 +19,25 @@ info = {
     "video": (YTS.Video.get, definitions.video_info)
 }
 
-youtube_argparser = ArgumentParser(description="""A project to easily search
-        for, and download videos from youtube""")
-youtube_argparser.add_argument("action", help="the action to take (i.e search, download, etc)")
-youtube_argparser.add_argument("query", help="arguments for the action.", nargs="*")
-youtube_argparser.add_argument("-f", "--fields", metavar="fs",
-                               help="specifies the fields to get from the video.")
-youtube_argparser.add_argument("-L", "--limit", metavar="L", help="the number of videos to find.",
-                               default=5)
-youtube_argparser.add_argument("-l", "--lang", metavar="l", help="filters the language.",
-                               default="en")
-youtube_argparser.add_argument("-r", "--region", metavar="r", help="filters the region.",
-                               default="US")
-youtube_argparser.add_argument("-m", "--no-menu", action="store_true",
-                               help="disables selection of results. displays in the console instead")
-youtube_argparser.add_argument("-c", "--clipboard",
-                               help="""store output in the clipboard. requires the
-                               xclip package on Linux.""",
-                               action="store_true")
-youtube_args = youtube_argparser.parse_args()
+yt_parser = ArgumentParser(description="""A project to easily search for, and
+                           download videos from youtube""")
+yt_parser.add_argument("action", help="""the action to take (i.e search,
+                       download, etc)""")
+yt_parser.add_argument("query", help="arguments for the action.", nargs="*")
+yt_parser.add_argument("-f", "--fields", help="""specifies th fields to get from
+                       the video.""", metavar="fs")
+yt_parser.add_argument("-L", "--limit", help="the number of videos to find.",
+                       metavar="L", default=5)
+yt_parser.add_argument("-l", "--lang", help="filters the language.",
+                       metavar="l", default="en")
+yt_parser.add_argument("-r", "--region", help="filters the region.",
+                       metavar="r", default="US")
+yt_parser.add_argument("-m", "--no-menu", help="""disables selection of results.
+                       displays in the console instead""", action="store_true")
+yt_parser.add_argument("-c", "--clipboard", help="""store links in the clipboard.
+                       requires the xclip package on Linux.""",
+                       action="store_true")
+youtube_args = yt_parser.parse_args()
 action = youtube_args.action.lower()
 
 # Make sure there are not too many arguments passed.
@@ -53,19 +54,22 @@ except KeyError:
 if action == "search":
     search_type, search_query = youtube_args.query
     search_type = search_type.lower()
-    
+
     try:
         search_function = search[search_type]
-        limit, language, region, = int(youtube_args.limit), youtube_args.lang, youtube_args.region
+        limit = int(youtube_args.limit)
+        language = youtube_args.lang
+        region = youtube_args.region
         results = search_function(search_query, limit=limit, language=language,
                                   region=region).result()["result"]
 
-        
         _, selected_results = definitions.select(results, ">>>: ",
                                                  definitions.search_format())
 
-        for result in selected_results:
-            print(result["link"])
+        # Copy the links to the clipboard.
+        if youtube_args.clipboard is True:
+            links = " ".join(info["link"] for info in selected_results)
+            clipboard.copy(links)
     except KeyError:
         print(f"{search_type} is not a valid search option.")
     except ValueError as e:
